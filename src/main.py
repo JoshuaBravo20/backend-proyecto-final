c3d28b47e86030d89d885a8c648729ef156f1d37
JoshuaBravo20 <69777229+JoshuaBravo20@users.noreply.github.com>
2021-02-19 11:56:44 -0300

    f

--- src/main.py
+++ src/main.py
@@ -41,5 +41,5 @@ def handle_hello():
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host='localhost', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -3,17 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    user_id = db.Column(db.String(100), unique=True, nullable=False, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    followers = db.Column(db.Integer, nullable=True)
+    """ chats = db.relationship('Chat', backref='user') """
 
     def serialize(self):
         return {
-            "id": self.id,
+            "user_id": self.user_id,
             "email": self.email,
+            "followers": self.followers
             # do not serialize the password, its a security breach
         }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update():
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+""" class Chats(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(100))
+    users_id = db.Column(db.String(100), db.ForeignKey("user.user_id", ondelete="CASCADE"), nullable=False) """
